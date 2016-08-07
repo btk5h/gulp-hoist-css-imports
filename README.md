1806f51255c536b936ed4022fae16015f51bdeb4
Bryan Terce <bryan.terce@gmail.com>
2016-08-06 23:04:15 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# gulp-hoist-css-imports
+
+[![JavaScript Style Guide](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat-square)](http://standardjs.com/)
+
+Moves CSS imports to the top of the file, wherever they may be located. This module was written to address the issue of css concatenation not producing valid css, but feel free to use this module if you just don't care where you put your imports ¯\\_(ツ)_/¯
+
+## Usage
+
+`hoistCssImports(options)`
+
+**options.matcher**
+Default: `/@import .*?;/g`
+The regex matching import statements. The default suits most use cases, but feel free to change this to match other css constructs.
+
+**options.delimiter**
+Default: `''`
+The delimiter appended to each import statement.
---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const through = require('through2')
+const {PluginError} = require('gulp-util')
+
+module.exports = ({matcher = /@import .*?;/g, delimiter = ''} = {}) => {
+  function transform (file, enc, cb) {
+    if (file.isNull()) {
+      return cb(null, file)
+    }
+
+    if (file.isStream()) {
+      return cb(new gutil.PluginError('gulp-hoist-css-imports', 'Streaming not supported'))
+    }
+
+    let imports = new Set()
+    let contents = file.contents.toString()
+
+    let match
+    while (match = matcher.exec(contents)) {
+      imports.add(match[0])
+    }
+
+    contents = new Buffer(contents.replace(matcher, ''))
+    imports = [...imports].map(i => new Buffer(`${i}${delimiter}`))
+
+    file.contents = Buffer.concat([...imports, contents])
+
+    cb(null, file)
+  }
+
+  return through.obj(transform)
+}

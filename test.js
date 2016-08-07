738ab14f01cf760d39e4170594933de9ff70c1a6
Bryan Terce <bryan.terce@gmail.com>
2016-08-07 10:38:40 -0700

    Look ma, I wrote a unit test!

---
+++ test.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import test from 'ava'
+import File from 'vinyl'
+import hoistImports from '.'
+
+test.cb('basic import hoisting', t => {
+  t.plan(1)
+
+  let testFile = new File({
+    contents: new Buffer('@import "firstImport"; css-rule {} @import "secondImport";')
+  })
+
+  let hoist = hoistImports()
+
+  hoist.write(testFile)
+
+  hoist.once('data', file => {
+    t.is(file.contents.toString(), '@import "firstImport";@import "secondImport"; css-rule {} ')
+    t.end()
+  })
+})
